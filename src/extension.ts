140490628c1b171c26831a25c17076c2ebeb9e56
quinnpan <quinnpan@tencent.com>
2021-03-12 18:17:42 +0800

    add logcat+

--- src/extension.ts
+++ src/extension.ts
@@ -19,8 +19,19 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from logcat+!');
 	});
+		// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable1 = vscode.commands.registerCommand('logcat-plus.logcat+', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from logcat+ +++!');
+	});
+
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable1);
 }
 
 // this method is called when your extension is deactivated
